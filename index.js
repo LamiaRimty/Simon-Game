a68e3ea9706b930162918f8144f2174d4a4243e2
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-04 17:14:58 +0200

    add animation & sound

--- index.js
+++ index.js
@@ -9,20 +9,29 @@ var gamePattern=[];
 function nextSequence(){
 
 var randomNumber=Math.floor(Math.random()*4); //2.inside the func generate a random number between 0-3,store it in variable called randomnumber
-console.log(randomNumber);
+var randomChosenColour=buttonColours[randomNumber];  // 6.Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+gamePattern.push(randomChosenColour);
+
+ //1. Use jQuery to select the button with the same id as the randomChosenColour
+ //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+
+ $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+//3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+var audio= new Audio("sounds/"+ randomChosenColour  +".mp3");
+audio.play();
+
 }
 nextSequence();
 
-var randomChosenColour=buttonColours[randomNumber];
 
 
-// 6.Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
-gamePattern.push(randomChosenColour);
 
 
-// $("#green").ready(() => {
-//     setInterval(() => {
-//         $('p').fadeToggle();
-     
-//     }, 500);
-// });
+// function buttonAnimation(currentKey){
+//    var activeButton = document.querySelector("."+currentKey); //button active changes the style that defined in css file
+//     activeButton.classList.toggle("pressed");
+//     setTimeout( function(){
+//         activeButton.classList.remove("pressed");
+//     },100 );//0.1 sec
+// }
